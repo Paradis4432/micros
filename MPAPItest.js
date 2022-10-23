5ee5c2d216ce77a78aae171c33dfdef7a3f5d957
Paradis <Lucasezequiel120202@gmail.com>
2022-10-22 22:10:46 -0300

    final v0

--- MPAPItest.js
+++ MPAPItest.js
@@ -1,15 +1,17 @@
 
 const request = require('request');
 const fs = require("fs")
+const FSDB = require("file-system-db");
+const db = new FSDB("./db.json", false);
 
 var options = {
-    url: 'https://api.mercadopago.com/v1/payments/search?sort=date_created&criteria=desc&access_token=APP_USR-1818826746292191-101710-33ff8190051418b5967d8e7e0f4f0d63-321320472&status=approved&offset=0&limit=10',
+    url: 'https://api.mercadopago.com/v1/payments/search?sort=date_created&criteria=desc&access_token=APP_USR-1818826746292191-101710-33ff8190051418b5967d8e7e0f4f0d63-321320472&status=approved&offset=0&limit=60',
     headers: { 'accept': 'application/json' }
 };
 
 function callback(error, response, body) {
     if (!error && response.statusCode == 200) {
-        
+
         fs.readdir("./payments", (err, files) => {
             let FLS = (files.length).toString()
             fs.writeFile("./payments/payment" + FLS + ".txt", body, (err) => {
@@ -18,8 +20,31 @@ function callback(error, response, body) {
         })
 
         const obj = JSON.parse(body)
-        console.log(obj)
+        for (let i = 0; i < obj["results"].length; i++) {
+            if(obj["results"][i].payer === undefined) continue
+            if(obj["results"][i].payer.email === undefined) continue
+            let mpmail = obj["results"][i].payer.email
+            let amount = obj["results"][i].transaction_amount
+
+            console.log(obj["results"][i].payer.email)
+            console.log(obj["results"][i].transaction_amount)
+            db.set("payments." + mpmail.replaceAll(".", "="), Math.round(amount))
+        }
     }
 }
 
+function del(){
+    db.deleteAll()
+}
+
+function a(){
+    let data = db.all()
+    for(let i = 0; i < data.length; i++){
+        console.log("--")
+        console.log(data[i]["data"])
+        var key = Object.keys(data[i]["data"])
+        console.log(key)
+    }
+}
+del()
 request(options, callback);
--- index.js
+++ index.js
@@ -15,26 +15,44 @@ app.get("/", (req, res) => {
     res.sendFile("./home.html", { root: __dirname })
 })
 
-app.get("/encontrar", (req,res) => {
-    res.sendFile("./encontrar.html", {root: __dirname})
+app.get("/encontrar", (req, res) => {
+    res.sendFile("./encontrar.html", { root: __dirname })
 })
 
 const FSDB = require("file-system-db");
-const db = new FSDB("./db.json", false); 
+const db = new FSDB("./db.json", false);
 
 app.post("/register", (req, res) => {
-    let nombre = req.body.nombre
-    let dni = req.body.dni
-    let mail = req.body.mail
-    let mpmail = req.body.mpmail
-    db.set(req.body.dni, {"nombre": nombre, "mail" : mail, "mpmail": mpmail})
+    let nombre = (req.body.nombre).replaceAll(".", "=")
+    let dni = (req.body.dni).replaceAll(".", "=")
+    let tel = (req.body.tel).replaceAll(".", "=")
+    let mpmail = (req.body.mpmail).replaceAll(".", "=")
+    // add check for existing mail 
+    db.set("registros." + mpmail, { "nombre": nombre, "dni": dni, "tel": tel })
 
-    res.sendFile("./gracias.html",{ root: __dirname})
+    res.sendFile("./gracias.html", { root: __dirname })
 })
 
-app.post("/encontrar", (req,res) => {
-    if(db.has(req.body.dni)) res.send("se encontro con nombre: " + db.get(req.body.dni + ".nombre"))
-    else req.send("no se encontro")
+app.post("/encontrar", (req, res) => {
+
+    // get dni
+    // search registros for any
+    // if match : search pay in payments 
+    // return dni , nombre, mpmail, amount payed,
+
+    let dni = (req.body.dni).replaceAll(".", "=")
+    var keys = Object.keys(db.get("registros"))
+    for (let i = 0; i < keys.length; i++) {
+        let key = keys[i]
+        if(g(key, "dni") == dni){
+            res.send("se encontro dni registrado con mail: " + key.replaceAll("=", ".") + " nombre: " + g(key, "nombre") + " telefono: " + g(key, "tel") + " monto pagado: " + db.get("payments." + key))
+            return
+        }
+    }
+    res.send("no se encontro")
 })
 
+function g(key, a){
+    return db.get("registros." + key + "." + a)
+}
 app.listen(80, () => console.log("server started in port 80"))
